611fc77df9afcbb1b40d5ce8ec543e34a3fdb21a
Justin Wilkins <justin.wilkins@goaptive.com>
2023-10-06 12:32:38 -0500

    Added in the AI API call

--- app/Http/Controllers/CustomerNoteController.php
+++ app/Http/Controllers/CustomerNoteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class CustomerNoteController extends Controller
 {
@@ -10,17 +11,40 @@ class CustomerNoteController extends Controller
     {
         $notes = $request->input('notes');
 
-        // TODO: All the logic for parsing the note then return the parsed data to a view that I will define
-
-        // Placeholder of what I am expecting
-        $parsedData = [
-            "pests" => [
-                ["pest" => "wasp", "location" => "eves"],
-                ["pest" => "ants", "location" => "backyard"]
-            ],
-            "note" => "I have a wasp nest in my eves and a terrible ant problem in my backyard"
-        ];
+        $parsedData = $this->submitToAI($notes);
+        $parsedData["note"] = $notes;
 
         return view('customer.pests', ['data' => $parsedData]);
+
+    }
+
+    private function submitToAI(string $notes): array
+    {
+        $authkey = "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";
+        $url = "https://api.openai.com/v1/chat/completions";
+
+        $requestData = [
+            "model" => "gpt-3.5-turbo",
+            "messages" => [
+                [
+                    "role" => "system",
+                    "content" => "You will be provided with unstructured data, and your task is to parse mentioned pests and their location into JSON"
+                ],
+                [
+                    "role" => "user",
+                    "content" => $notes
+                ]
+                ],
+            "temperature" => 0,
+            "max_tokens" => 256,
+            "top_p" => 1,
+            "frequency_penalty" => 0,
+            "presence_penalty" => 0
+        ];
+
+        $response = Http::withToken($authkey)
+            ->accept('application/json')
+            ->post($url, $requestData);
+        return json_decode($response->json()["choices"][0]["message"]["content"], true);
     }
 }
